65659e4af487ccbff282b4101961795a33a56271
Daniel Mynarski <daniel.mynarski@ge.com>
2020-01-22 23:31:58 +0100

    add devise for auth

--- ticketsRuby/config/routes.rb
+++ ticketsRuby/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # get 'events/index'
   # get 'events/new'
   # post 'events/new' => 'events#create'
@@ -6,17 +7,14 @@ Rails.application.routes.draw do
   # get 'events/:id' => 'events#show'
   # get 'events/:id' => 'events#show'
 
-
   root :to => "events#index"
 
   resources :tickets
   resources :events, :only => [:index, :new, :create, :show]
 
-
   # resources :events
   # resources :tickets
   # root :to => "tickets#index"
-
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- ticketsRuby/db/schema.rb
+++ ticketsRuby/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_20_234254) do
+ActiveRecord::Schema.define(version: 2020_01_22_215808) do
 
   create_table "events", force: :cascade do |t|
     t.string "artist"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_01_20_234254) do
     t.decimal "price_low"
     t.decimal "price_high"
     t.date "event_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "tickets", force: :cascade do |t|
@@ -31,6 +33,18 @@ ActiveRecord::Schema.define(version: 2020_01_20_234254) do
     t.string "phone"
     t.integer "event_id"
     t.index ["event_id"], name: "index_tickets_on_event_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "tickets", "events"
