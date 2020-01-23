911586dc9a70d1897eca786ada34fde83f1bc882
Daniel Mynarski <daniel.mynarski@ge.com>
2020-01-23 02:02:00 +0100

    final cleaning

--- ticketsRuby/app/controllers/application_controller.rb
+++ ticketsRuby/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  protected
+
+  def after_sign_in_path_for(resource)
+    tickets_path
+  end
+  def after_sign_out_path_for(resource)
+    tickets_path
+  end
 end
